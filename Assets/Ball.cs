5a673ccfd6f94864da8ae51ab1739523f3e14f36
hopestrikestwice <54427372+hopestrikestwice@users.noreply.github.com>
2021-02-22 13:48:27 -0500

    basic scene added

---
+++ Assets/Ball.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField]
+    private Text ScoreBoard;
+
+    private int player1Score, player2Score = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody>().AddForce(transform.right * -100.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ScoreBoard.text = "Player 1: " + player1Score + ", Player 2: " + player2Score;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player 1 Goal"))
+        {
+            player1Score++;
+        }
+        else if (other.gameObject.CompareTag("Player 2 Goal"))
+        {
+            player2Score++;
+        }
+
+        resetBall();
+    }
+
+    private void resetBall()
+    {
+        this.transform.position = new Vector3(0, 0, 0);
+    }
+}
---
+++ Assets/Paddle1Controller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle1Controller : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 360f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.up * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.up * speed * Time.deltaTime);
+        }
+    }
+}
