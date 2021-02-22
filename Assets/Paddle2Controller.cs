31629e27213446aa36bbc47363059ebf758d852c
hopestrikestwice <54427372+hopestrikestwice@users.noreply.github.com>
2021-02-22 13:48:27 -0500

    basic scene added

---
+++ Assets/Paddle2Controller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle2Controller : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 360f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.up * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(-Vector3.up * speed * Time.deltaTime);
+        }
+    }
+}
