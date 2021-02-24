8f94d4fece9cbf088751a661a27c327b9da34489
hopestrikestwice <54427372+hopestrikestwice@users.noreply.github.com>
2021-02-23 19:29:36 -0500

    Full board of 6 unit, 4 paddles, evenly spaced. Linear movement with roughly correct end restrictions

--- Assets/Paddle1Controller.cs
+++ Assets/Paddle1Controller.cs
@@ -4,19 +4,39 @@ using UnityEngine;
 
 public class Paddle1Controller : MonoBehaviour
 {
-    [SerializeField]
-    private float speed = 360f;
+    private float rotSpeed = 360f;
+    private float linSpeed = 8f;
+
+    private float maxDisplacement = 2f;
+
+    private float currentDisplacement = 0f;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.W) && currentDisplacement < maxDisplacement)
         {
-            transform.Rotate(Vector3.up * speed * Time.deltaTime);
+            Vector3 translation = Vector3.up * linSpeed * Time.deltaTime;
+            transform.Translate(translation);
+
+            currentDisplacement += translation.magnitude;
         }
+
+        if (Input.GetKey(KeyCode.S) && currentDisplacement > -maxDisplacement)
+        {
+            Vector3 translation = Vector3.down * linSpeed * Time.deltaTime;
+            transform.Translate(translation);
+
+            currentDisplacement -= translation.magnitude;
+        }
+
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(-Vector3.up * speed * Time.deltaTime);
+            transform.Rotate(Vector3.up * rotSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(-Vector3.up * rotSpeed * Time.deltaTime);
         }
     }
 }
--- Assets/Paddle2Controller.cs
+++ Assets/Paddle2Controller.cs
@@ -4,19 +4,39 @@ using UnityEngine;
 
 public class Paddle2Controller : MonoBehaviour
 {
-    [SerializeField]
-    private float speed = 360f;
+    private float rotSpeed = 360f;
+    private float linSpeed = 8f;
+
+    private float maxDisplacement = 2f;
+
+    private float currentDisplacement = 0f;
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKey(KeyCode.UpArrow) && currentDisplacement < maxDisplacement)
+        {
+            Vector3 translation = Vector3.down * linSpeed * Time.deltaTime;
+            transform.Translate(translation);
+
+            currentDisplacement += translation.magnitude;
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow) && currentDisplacement > -maxDisplacement)
+        {
+            Vector3 translation = Vector3.up * linSpeed * Time.deltaTime;
+            transform.Translate(translation);
+
+            currentDisplacement -= translation.magnitude;
+        }
+
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.Rotate(Vector3.up * speed * Time.deltaTime);
+            transform.Rotate(Vector3.up * rotSpeed * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.Rotate(-Vector3.up * speed * Time.deltaTime);
+            transform.Rotate(-Vector3.up * rotSpeed * Time.deltaTime);
         }
     }
 }
