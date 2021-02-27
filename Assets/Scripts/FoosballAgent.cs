57ffa557aa4bcbf3c296f8a04037fb4d51293f7b
Edward Li <edwardyaoli@gmail.com>
2021-02-27 05:34:28 -0500

    initial training test

--- Assets/Scripts/FoosballAgent.cs
+++ Assets/Scripts/FoosballAgent.cs
@@ -9,6 +9,7 @@ using Unity.MLAgents.Actuators;
 public class FoosballAgent : Agent {
     public GameObject ball;
     public Transform goal;
+    public Transform opponentGoal;
 
     public bool player1;
     public GameObject offense;
@@ -16,14 +17,16 @@ public class FoosballAgent : Agent {
 
     //TODO: Tune these
     float linMoveMultiplier = 0.25f;
-    float angMoveMultiplier = 5f;
+    float angMoveMultiplier = 10f;
 
-    float ballStartMultiplier = 2f; //20f;
+    float ballStartMultiplier = 20f; //20f;
 
-    float maxLinMove = 2f;
+    float maxLinMove = 4.64f;
 
     float frozenTime = 0f;
     float maxFrozenTime = 10f;
+    
+    float invert;
 
     // Start is called before the first frame update
     void Start() {
@@ -39,49 +42,58 @@ public class FoosballAgent : Agent {
     }
 
     public override void CollectObservations(VectorSensor sensor) {
-        //Ball Observations
-        sensor.AddObservation(ball.transform.localPosition);
-        sensor.AddObservation(ball.GetComponent<Rigidbody>().velocity.x);
-        sensor.AddObservation(ball.GetComponent<Rigidbody>().velocity.y);
-        sensor.AddObservation(ball.GetComponent<Rigidbody>().velocity.z);
+        //Ball Observation
+        invert = player1 ? 1f : -1f;
+
+        sensor.AddObservation(invert * ball.transform.localPosition.x);
+        sensor.AddObservation(invert * ball.transform.localPosition.y);
+        sensor.AddObservation(invert * ball.GetComponent<Rigidbody>().velocity.x);
+        sensor.AddObservation(invert * ball.GetComponent<Rigidbody>().velocity.y);
+        //sensor.AddObservation(ball.GetComponent<Rigidbody>().velocity.z);
         //Possibly add observe goal position?
 
         //Paddle Observations
-        sensor.AddObservation(offense.transform.localPosition);
-        sensor.AddObservation(offense.transform.localRotation);
-        sensor.AddObservation(goalie.transform.localPosition);
-        sensor.AddObservation(goalie.transform.localRotation);
+        sensor.AddObservation(offense.transform.localPosition.z);
+        sensor.AddObservation(offense.transform.localRotation.z);
+        sensor.AddObservation(goalie.transform.localPosition.z);
+        sensor.AddObservation(goalie.transform.localRotation.z);
     }
 
     public override void OnActionReceived(ActionBuffers actions) {
         //Actions, size = 4
-        Vector3 offenseLinMove = Vector3.up * actions.ContinuousActions[0] * linMoveMultiplier;
+        Vector3 offenseLinMove = Vector3.forward * actions.ContinuousActions[0] * linMoveMultiplier;
         
-        if (player1 && Mathf.Abs(offense.transform.localPosition.z + offenseLinMove.y) < maxLinMove) {
+        if (player1 && Mathf.Abs(offense.transform.localPosition.z + offenseLinMove.z) < maxLinMove) {
             offense.transform.Translate(offenseLinMove);
-        } else if (!player1 && Mathf.Abs(offense.transform.localPosition.z - offenseLinMove.y) < maxLinMove) {
+        } else if (!player1 && Mathf.Abs(offense.transform.localPosition.z + offenseLinMove.z) < maxLinMove) {
             offense.transform.Translate(offenseLinMove);
         }
 
-        Vector3 offenseAngMove = Vector3.up * actions.ContinuousActions[1] * angMoveMultiplier;
+        Vector3 offenseAngMove = Vector3.forward * actions.ContinuousActions[1] * angMoveMultiplier;
         offense.transform.Rotate(offenseAngMove);
 
-        Vector3 goalieLinMove = Vector3.up * actions.ContinuousActions[2] * linMoveMultiplier;
-        if (player1 && Mathf.Abs(goalie.transform.localPosition.z + goalieLinMove.y) < maxLinMove) {
+        Vector3 goalieLinMove = Vector3.forward * actions.ContinuousActions[2] * linMoveMultiplier;
+        if (player1 && Mathf.Abs(goalie.transform.localPosition.z + goalieLinMove.z) < maxLinMove) {
             goalie.transform.Translate(goalieLinMove);
-        } else if (!player1 && Mathf.Abs(goalie.transform.localPosition.z - goalieLinMove.y) < maxLinMove) {
+        } else if (!player1 && Mathf.Abs(goalie.transform.localPosition.z + goalieLinMove.z) < maxLinMove) {
             goalie.transform.Translate(goalieLinMove);
         }
 
-        Vector3 goalieAngMove = Vector3.up * actions.ContinuousActions[3] * angMoveMultiplier;
+        Vector3 goalieAngMove = Vector3.forward * actions.ContinuousActions[3] * angMoveMultiplier;
         goalie.transform.Rotate(goalieAngMove);
 
         //Rewards
         float distanceToGoal = Vector3.Distance(ball.transform.localPosition, goal.localPosition);
         //Reached Target. TODO: Change this to work with the trigger  instead:
-        if (distanceToGoal <  1.42f) {
+        if (distanceToGoal <  3f) {
             SetReward(1.0f);
             EndEpisode();
+        }
+
+        float distanceToOpponentGoal = Vector3.Distance(ball.transform.localPosition, opponentGoal.localPosition);
+        if (distanceToOpponentGoal <  3f) {
+            SetReward(-1.0f);
+            //EndEpisode();
         }
 
         //TODO: neg reward for owngoal? Reset episode if ball falls off?
@@ -102,5 +114,12 @@ public class FoosballAgent : Agent {
 
         continuousActionsOut[2] = Input.GetAxis("Vertical");
         continuousActionsOut[3] = Input.GetAxis("Horizontal");
+
+        if (!player1) {
+            continuousActionsOut[0] *= -1f;
+            continuousActionsOut[1] *= -1f;
+            continuousActionsOut[2] *= -1f;
+            continuousActionsOut[3] *= -1f;
+        }
     }
 }
