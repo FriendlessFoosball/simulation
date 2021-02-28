38133cb6ed9210775217971fa2309f2c4b42d8f5
Edward Li <edwardyaoli@gmail.com>
2021-02-28 03:49:49 -0500

    much better reward management

---
+++ Assets/Scripts/BallScorer.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScorer : MonoBehaviour
+{
+    public GameObject humanAgent;
+    public GameObject humanOffense;
+    public GameObject humanGoalie;
+    public GameObject robotAgent;
+    public GameObject robotOffense;
+    public GameObject robotGoalie;
+    
+    FoosballAgent m_humanAgent;
+    FoosballAgent m_robotAgent;
+
+    PaddleInitializer m_humanOffense;
+    PaddleInitializer m_humanGoalie;
+    PaddleInitializer m_robotOffense;
+    PaddleInitializer m_robotGoalie;
+
+    Rigidbody rb;
+
+    const float fieldWidth = 12.835f;
+    const float fieldLength = 18.47f;
+    const float ballStartMultiplier = 1000f; //20f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_humanAgent = humanAgent.GetComponent<FoosballAgent>();
+        m_robotAgent = robotAgent.GetComponent<FoosballAgent>();
+
+        m_humanOffense = humanOffense.GetComponent<PaddleInitializer>();
+        m_humanGoalie = humanGoalie.GetComponent<PaddleInitializer>();
+        m_robotOffense = robotOffense.GetComponent<PaddleInitializer>();
+        m_robotGoalie = robotGoalie.GetComponent<PaddleInitializer>();
+        
+        rb = GetComponent<Rigidbody>();
+
+        ResetBall();
+    }
+
+    void Reset()
+    {
+        m_humanAgent.EndEpisode();
+        m_robotAgent.EndEpisode();
+
+        m_humanOffense.Reset();
+        m_humanGoalie.Reset();
+        m_robotOffense.Reset();
+        m_robotGoalie.Reset();
+
+        ResetBall();
+    }
+
+    public void ResetBall()
+    {
+        transform.localPosition = Vector3.zero;
+        rb.velocity = Vector3.zero;
+        rb.AddRelativeForce(Random.onUnitSphere * ballStartMultiplier);
+    }
+
+    void HumanWin()
+    {
+        m_humanAgent.SetReward(1f);
+        m_robotAgent.SetReward(-1f);
+        Reset();
+    }
+
+    void RobotWin()
+    {
+        m_humanAgent.SetReward(-1f);
+        m_robotAgent.SetReward(1f);
+        Reset();
+    }
+
+    void Failure()
+    {
+        m_humanAgent.SetReward(-4f);
+        m_robotAgent.SetReward(-4f);
+        Reset();
+    }
+
+    void OnTriggerEnter(Collider collision) {
+        if (collision.gameObject.CompareTag("Robot"))
+            RobotWin();
+        else if (collision.gameObject.CompareTag("Human"))
+            HumanWin();
+    }
+
+    void FixedUpdate()
+    {
+        if (transform.localPosition.y < -5 || Mathf.Abs(transform.localPosition.x) > (fieldLength + 5) || Mathf.Abs(transform.localPosition.z) > (fieldWidth + 5))
+            Failure();
+    }
+}
--- Assets/Scripts/FoosballAgent.cs
+++ Assets/Scripts/FoosballAgent.cs
@@ -20,16 +20,14 @@ public class FoosballAgent : Agent {
 
     //TODO: Tune these
     const float linMoveMultiplier = 2f;//0.25f;
-    const float angMoveMultiplier = 5f; //10f;
+    const float angMoveMultiplier = 20f; //10f;
 
     const float maxVelocity = 16f;
 
-    const float ballStartMultiplier = 400f; //20f;
-
     const float maxLinMove = 4.64f;
 
-    float frozenTime = 0f;
-    const float maxFrozenTime = 10f;
+    //float frozenTime = 0f;
+    //const float maxFrozenTime = 10f;
 
     const float maxAngularVelocity = 30f;
     const float fieldWidth = 12.835f;
@@ -46,10 +44,7 @@ public class FoosballAgent : Agent {
     Rigidbody oOffenseRb;
     Rigidbody oGoalieRb;
 
-    PaddleInitializer offenseScript;
-    PaddleInitializer goalieScript;
-    PaddleInitializer oOffenseScript;
-    PaddleInitializer oGoalieScript;
+    BallScorer bs;
 
     // Start is called before the first frame update
     void Start() {
@@ -60,25 +55,15 @@ public class FoosballAgent : Agent {
         oOffenseRb = opponentOffense.GetComponent<Rigidbody>();
         oGoalieRb = opponentGoalie.GetComponent<Rigidbody>();
 
-        offenseScript = offense.GetComponent<PaddleInitializer>();
-        goalieScript = goalie.GetComponent<PaddleInitializer>();
-        oOffenseScript = opponentOffense.GetComponent<PaddleInitializer>();
-        oGoalieScript = opponentGoalie.GetComponent<PaddleInitializer>();
+        bs = ball.GetComponent<BallScorer>();
+
+        invert = player1 ? 1f : -1f;
     }
 
     //Reset ball to middle with random force
     public override void OnEpisodeBegin() {
-        frozenTime = 0f;
-        ball.transform.localPosition = Vector3.zero;
-        ballRb.velocity = Vector3.zero;
-        ballRb.AddRelativeForce(Random.onUnitSphere * ballStartMultiplier);
-
-        offenseScript.Reset();
-        goalieScript.Reset();
-        oOffenseScript.Reset();
-        oGoalieScript.Reset();
-
-        invert = player1 ? 1f : -1f;
+        // backup for max timestep
+        bs.ResetBall();
     }
 
     public override void CollectObservations(VectorSensor sensor) {
@@ -110,12 +95,6 @@ public class FoosballAgent : Agent {
     public override void OnActionReceived(ActionBuffers actions) {
         //Actions, size = 4
         Vector3 offenseLinMove = Vector3.forward * actions.ContinuousActions[0] * linMoveMultiplier;
-        
-        // if (player1 && Mathf.Abs(offense.transform.localPosition.z + offenseLinMove.z) < maxLinMove) {
-        //     offense.transform.Translate(offenseLinMove);
-        // } else if (!player1 && Mathf.Abs(offense.transform.localPosition.z + offenseLinMove.z) < maxLinMove) {
-        //     offense.transform.Translate(offenseLinMove);
-        // }
 
         if (Mathf.Abs(invert * offenseRb.velocity.z + actions.ContinuousActions[0] * linMoveMultiplier) > maxVelocity) {
             Vector3 maxSpeed = Vector3.forward * maxVelocity;
@@ -126,15 +105,9 @@ public class FoosballAgent : Agent {
         offenseRb.AddRelativeForce(offenseLinMove, ForceMode.VelocityChange);
 
         Vector3 offenseAngMove = Vector3.forward * actions.ContinuousActions[1] * angMoveMultiplier;
-        //offense.transform.Rotate(offenseAngMove);
         offenseRb.AddRelativeTorque(offenseAngMove, ForceMode.VelocityChange);
 
         Vector3 goalieLinMove = Vector3.forward * actions.ContinuousActions[2] * linMoveMultiplier;
-        // if (player1 && Mathf.Abs(goalie.transform.localPosition.z + goalieLinMove.z) < maxLinMove) {
-        //     goalie.transform.Translate(goalieLinMove);
-        // } else if (!player1 && Mathf.Abs(goalie.transform.localPosition.z + goalieLinMove.z) < maxLinMove) {
-        //     goalie.transform.Translate(goalieLinMove);
-        // }
 
         if (Mathf.Abs(invert * goalieRb.velocity.z + actions.ContinuousActions[2] * linMoveMultiplier) > maxVelocity) {
             Vector3 maxSpeed = Vector3.forward * maxVelocity;
@@ -145,42 +118,11 @@ public class FoosballAgent : Agent {
         goalieRb.AddRelativeForce(goalieLinMove, ForceMode.VelocityChange);
 
         Vector3 goalieAngMove = Vector3.forward * actions.ContinuousActions[3] * angMoveMultiplier;
-        // goalie.transform.Rotate(goalieAngMove);
         goalieRb.AddRelativeTorque(goalieAngMove, ForceMode.VelocityChange);
-
-        //Rewards
-        float distanceToGoal = Vector3.Distance(ball.transform.localPosition, goal.localPosition);
-        //Reached Target. TODO: Change this to work with the trigger  instead:
-        if (distanceToGoal <  3f) {
-            SetReward(1.0f);
-            EndEpisode();
-        }
-
-        float distanceToOpponentGoal = Vector3.Distance(ball.transform.localPosition, opponentGoal.localPosition);
-        if (distanceToOpponentGoal <  3f) {
-            SetReward(-1.0f);
-            //EndEpisode();
-        }
-
-        //TODO: neg reward for owngoal? Reset episode if ball falls off?
-        //Reset if ball stops moving for 10 secs or falls off plane
-        // if (ballRb.IsSleeping() || ball.transform.position.y < -5) {
-        //     if (frozenTime != 0f && Time.time - frozenTime > maxFrozenTime) {
-        //         SetReward(-1.0f);
-        //         EndEpisode();
-        //     } else if (frozenTime == 0f) {
-        //         frozenTime = Time.time;
-        //     }
-        // }
-        
-        if (ball.transform.localPosition.y < -5 || Mathf.Abs(ball.transform.localPosition.x) > (fieldLength + 5) || Mathf.Abs(ball.transform.localPosition.z) > (fieldWidth + 5)) {
-            SetReward(-4.0f);
-            EndEpisode();
-        }
 
         float spin = Mathf.Abs(offenseRb.angularVelocity.z / maxAngularVelocity) + Mathf.Abs(goalieRb.angularVelocity.z / maxAngularVelocity);
 
-        AddReward(-0.001f * 0.50f * spin);
+        AddReward(-0.01f * 0.50f * spin);
         //AddReward(-0.001f);
     }
 
